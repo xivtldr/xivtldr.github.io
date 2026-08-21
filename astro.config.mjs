b63b0e0e2cc10c5f45c6c3f9bffca07123fbdc33
paru-de <13712334+paru-de@users.noreply.github.com>
2026-08-21 20:15:00 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,7 +43,7 @@ export default defineConfig({
         },
       ],
       editLink: {
-        baseUrl: 'https://github.com/xivtldr/xivtldr.github.io/edit/main/src/content/docs',
+        baseUrl: 'https://github.com/xivtldr/xivtldr.github.io/edit/main/',
       },
       defaultLocale: 'root',
       locales: {
